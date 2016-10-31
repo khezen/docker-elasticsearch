2b3c1b2f03b5da1be0870a394c0af4c55698d32b
Guillaume Simonneau <milu@localhost.localdomain>
2016-10-31 12:34:50 +0100

    fixing wait_until_started.sh

--- src/miscellaneous/wait_until_started.sh
+++ src/miscellaneous/wait_until_started.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-echo "Stalling for Elasticsearch"
-while true; do
-    nc -q 1 $elasticsearch_host $elasticsearch_port 2>/dev/null && break
-done
-
-echo "Elasticsearch started"
+sleep 20
