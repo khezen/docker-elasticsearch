e4267c6825f2782a99362df9e047f400119adbbf
Guillaume Simonneau <milu@localhost.localdomain>
2016-11-01 16:53:17 +0100

    smart waiting until started

--- src/miscellaneous/wait_until_started.sh
+++ src/miscellaneous/wait_until_started.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
-sleep 20
+RET=1
+while [[ RET -ne 0 ]]; do
+    echo "Stalling for Elasticsearch..."
+    sleep 5
+     curl -XGET "http://localhost:9200/" >/dev/null 2>&1
+    RET=$?
+done
