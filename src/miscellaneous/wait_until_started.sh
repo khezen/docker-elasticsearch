eb6f7af27b98a2540dc9a3a38d5bd3987b08b946
Guillaume <guillaume@localhost.localdomain>
2016-11-03 10:22:37 +0100

    sleeping after testing

--- src/miscellaneous/wait_until_started.sh
+++ src/miscellaneous/wait_until_started.sh
@@ -2,7 +2,7 @@
 RET=1
 while [[ RET -ne 0 ]]; do
     echo "Stalling for Elasticsearch..."
-    sleep 5
-     curl -XGET "http://localhost:9200/" >/dev/null 2>&1
+    curl -XGET "http://localhost:9200/" >/dev/null 2>&1
     RET=$?
+    sleep 5
 done
