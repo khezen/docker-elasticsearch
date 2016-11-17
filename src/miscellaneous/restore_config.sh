a78272c7178ed781aceb8ffe12836272cf3435a7
Guillaume <guillaume@localhost.localdomain>
2016-11-17 16:56:34 +0100

    moving config file

--- src/miscellaneous/restore_config.sh
+++ src/miscellaneous/restore_config.sh
@@ -2,5 +2,5 @@
  
 export CONF_DIR=/etc/elasticsearch/config/elasticsearch.yml
 if [ ! -f /etc/elasticsearch/config/elasticsearch.yml ]; then
-    cp -r /.backup/config /etc/elasticsearch/config
+    cp -r /.backup/config /etc/elasticsearch/
 fi
