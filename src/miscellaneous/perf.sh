04940bb62600b19929034332e545cc41b032ebc3
Guillaume <guillaume@localhost.localdomain>
2016-11-17 18:15:26 +0100

    add conf to es_java_opt

--- src/miscellaneous/perf.sh
+++ src/miscellaneous/perf.sh
@@ -2,4 +2,4 @@
 
 sysctl -w vm.max_map_count=262144
 sysctl -w vm.swappiness=1
-export ES_JAVA_OPTS="-Xms$HEAP_SIZE -Xmx$HEAP_SIZE"
+export ES_JAVA_OPTS=$ES_JAVA_OPTS" -Xms$HEAP_SIZE -Xmx$HEAP_SIZE"
--- src/miscellaneous/restore_config.sh
+++ src/miscellaneous/restore_config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ES_JAVA_OPTS=$ES_JAVA_OPTS" -Des.path.conf=$CONF_DIR"
 if [ ! -f /etc/elasticsearch/config/elasticsearch.yml ]; then
     cp -r /.backup/config/elasticsearch.yml $CONF_DIR/elasticsearch.yml
 fi
