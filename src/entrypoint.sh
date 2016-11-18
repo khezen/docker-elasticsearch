6275364439a7ddc3f6c2550691685e3f831e6dbf
Guillaume Simonneau <milu@localhost.localdomain>
2016-11-18 16:14:37 +0100

    fixing HEAP_SIZE managment

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 set -m
-ES_JAVA_OPTS="-Xms$HEAP_SIZE -Xmx$HEAP_SIZE"
 /run/miscellaneous/restore_config.sh
 
 # Run as user "elasticsearch" if the command is "elasticsearch"
 if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
 	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
 	set -- gosu elasticsearch "$@"
+	ES_JAVA_OPTS="-Xms$HEAP_SIZE -Xmx$HEAP_SIZE" $@ &
+else
+	$@ &
 fi
-$@ &
 
 /run/miscellaneous/wait_until_started.sh
 
