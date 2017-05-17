2c2d3655d3a373a989c0445fc57a0422a7ef5cbb
khezen <simonneaug@gmail.com>
2017-05-17 19:14:23 +0200

    #14 add indeices:read* to kibana role

--- src/miscellaneous/wait_until_started.sh
+++ src/miscellaneous/wait_until_started.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 RET=1
+
+http="http"
+if [ $HTTP_SSL ]; then
+  http="https"
+fi
 while [[ RET -ne 0 ]]; do
     echo "Stalling for Elasticsearch..."
-    curl -XGET -k -u "elastic:$$ELASTIC_PWD" "https://localhost:9200/" >/dev/null 2>&1
+    curl -XGET -k -u "elastic:$$ELASTIC_PWD" "$http://localhost:9200/" >/dev/null 2>&1
     RET=$?
     sleep 5
 done
