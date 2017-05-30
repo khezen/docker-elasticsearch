52f26da93427a1e615cbf61070c693e683801f6f
khezen <simonneaug@gmail.com>
2017-05-30 12:28:50 +0200

    #11 - index level settings support https

--- src/miscellaneous/index_level_settings.sh
+++ src/miscellaneous/index_level_settings.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-curl -XPUT -k -u "elastic:$$ELASTIC_PWD" 'http://localhost:9200/_all/_settings?preserve_existing=true' -d '{
+http="http"
+if [ $HTTP_SSL ]; then
+  http="https"
+fi
+
+curl -XPUT -k -u "elastic:$ELASTIC_PWD" "$http://localhost:9200/_all/_settings?preserve_existing=true" -d '{
     "index.auto_expand_replicas" : "0-all"
 }'
