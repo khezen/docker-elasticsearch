47d3df959ed7d81fa0f0b8a56eedb787064cb7f4
Guillaume Simonneau <2980507+khezen@users.noreply.github.com>
2019-04-11 19:46:55 +0200

    backup jvm.options

--- src/miscellaneous/restore_config.sh
+++ src/miscellaneous/restore_config.sh
@@ -9,5 +9,9 @@ if [ ! -f /elasticsearch/config/log4j2.properties ]; then
     cp -r /.backup/elasticsearch/config/log4j2.properties /elasticsearch/config/
 fi
 
+if [ ! -f /elasticsearch/config/jvm.options ]; then
+    cp -r /.backup/elasticsearch/config/jvm.options /elasticsearch/config/
+fi
+
 
 rsync -av --ignore-existing /.backup/elasticsearch/config/searchguard/ /elasticsearch/config/searchguard/
